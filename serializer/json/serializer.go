09508ecc457a7bd3fa89d2c5ad192281037abfff
Marcellus Easley <marcellus.easley@gmail.com>
2024-02-13 21:38:53 -0500

    add json serializer

---
+++ serializer/json/serializer.go
@@ -0,0 +1,25 @@
+package json 
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"github.com/marcelluseasley/hex-ms-poc-1/shortener"
+)
+
+type Redirect struct{}
+
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	bytes, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Encode")
+	}
+	return bytes, nil
+}
