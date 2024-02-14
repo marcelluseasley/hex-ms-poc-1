068cce88eb0c34de52a15e0e2451dab402a726d3
Marcellus Easley <marcellus.easley@gmail.com>
2024-02-13 21:55:30 -0500

    add mongodb repository

---
+++ repository/mongodb/repository.go
@@ -0,0 +1,76 @@
+package mongodb 
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/marcelluseasley/hex-ms-poc-1/shortener"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type mongoRepository struct {
+	client *mongo.Client
+	db     string
+	timeout time.Duration
+}
+
+func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout)*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.NewMongoClient")
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.NewMongoClient")
+	}
+	return client, nil
+}
+
+func NewMongoRepository(mongoURL, mongoDB string, mongoTimeout int) (shortener.RedirectRepository, error) {
+	repo := &mongoRepository{
+		timeout: time.Duration(mongoTimeout) * time.Second,
+		db:     mongoDB,
+	}
+	client, err := newMongoClient(mongoURL, mongoTimeout)
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.NewMongoRepository")
+	}
+	repo.client = client
+	return repo, nil
+}
+
+func (r *mongoRepository) Find(code string) (*shortener.Redirect, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	defer cancel()
+	redirect := &shortener.Redirect{}
+	collection := r.client.Database(r.db).Collection("redirects")
+	filter := bson.M{"code": code}
+	err := collection.FindOne(ctx, filter).Decode(redirect)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.Wrap(shortener.ErrRedirectNotFound, "repository.Redirect.Find")
+		}
+		return nil, errors.Wrap(err, "repository.Redirect.Find")
+	}
+	return redirect, nil
+}
+
+func (r *mongoRepository) Store(redirect *shortener.Redirect) error {
+	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	defer cancel()
+	collection := r.client.Database(r.db).Collection("redirects")
+	_, err := collection.InsertOne(ctx, redirect)
+	if err != nil {
+		return errors.Wrap(err, "repository.Redirect.Store")
+	}
+	return nil
+}
+
+
+
